13c56029fadb8877941c6e4d5bfdd31a25ccacfb
Vitali Filippov <vitaliy.filippov.2012@mail.ru>
2020-02-27 00:02:05 +0300

    Add lab3_a

---
+++ Lab3/Lab3/Default_first.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default_first.aspx.cs" Inherits="Lab3.Default_first" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        input, textarea, select {
+            left: 240px;
+            position: absolute;
+        }
+
+        #Radio2 { left: 260px; }
+
+        #Select { width: 160px; }
+    </style>
+</head>
+<body>
+<form id="form1" runat="server">
+    <div>
+        <p>
+            Элемент HTMLInputReset
+            <input id="ResetButton" type="reset" value="Reset" runat="server"/>
+        </p>
+
+        <p>
+            Элемент HtmlInputButton
+            <input id="Button" onserverclick="Button_ServerClick" type="button" value="Button" runat="server"/>
+        </p>
+
+        <p>
+            Элемент HtmlInputSubmit
+            <input id="SubmitButton" onserverclick="SubmitButton_ServerClick" type="submit" value="Submit" runat="server"/>
+        </p>
+
+        <p>
+            Элемент HtmlInputFile
+            <input type="file" runat="server"/>
+        </p>
+
+        <p>
+            Элемент HtmlInputText
+            <input id="InputText" onserverchange="InputText_ServerChange" type="text" runat="server"/>
+        </p>
+
+        <p>
+            Элемент HtmlInputPassword
+            <input id="InputPassword" type="password" runat="server"/>
+        </p>
+
+        <p>
+            Элемент HtmlInputCheckbox
+            <input id="CheckBox" onserverchange="CheckBox_ServerChange" type="checkbox" runat="server"/>
+        </p>
+
+        <p>
+            Элемент HtmlInputRadioButton
+            <input id="Radio1" onserverchange="Radio1_ServerChange" type="radio" runat="server"/>
+            <input id="Radio2" onserverchange="Radio2_ServerChange" type="radio" runat="server"/>
+        </p>
+
+        <p>
+            Элемент HtmlTextArea
+            <textarea id="TextArea" onserverchange="TextArea_ServerChange" runat="server" />
+        </p>
+
+        <p>
+            Элемент HtmlSelect
+            <select id="Select" onserverchange="Select_ServerChange" runat="server"/>
+        </p>
+    </div>
+</form>
+</body>
+</html>
---
+++ Lab3/Lab3/Default_first.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+
+namespace Lab3
+{
+    public partial class Default_first : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+                return;
+
+            Select.Items.Add("Item 1");
+            Select.Items.Add("Item 2");
+            Select.Items.Add("Item 3");
+
+            var htmlTable = new HtmlTable
+            {
+                Border = 1,
+                CellPadding = 3,
+                CellSpacing = 3,
+                Align = "center",
+                BorderColor = "black"
+            };
+
+            htmlTable.Style.Add("border-collapse", "collapse");
+            htmlTable.ID = "HtmlTable";
+
+            var titlesRow = new HtmlTableRow();
+
+            var cell = new HtmlTableCell {InnerHtml = "Html Control"};
+            titlesRow.Cells.Add(cell);
+
+            cell = new HtmlTableCell {InnerHtml = "OnServerClick"};
+            titlesRow.Cells.Add(cell);
+
+            cell = new HtmlTableCell {InnerHtml = "OnServerChange"};
+            titlesRow.Cells.Add(cell);
+
+            htmlTable.Rows.Add(titlesRow);
+
+            var resetButtonRow = new HtmlTableRow();
+
+            cell = new HtmlTableCell {InnerHtml = "HtmlInputReset"};
+            resetButtonRow.Cells.Add(cell);
+
+            cell = new HtmlTableCell {InnerHtml = "NO"};
+            resetButtonRow.Cells.Add(cell);
+
+            cell = new HtmlTableCell {InnerHtml = "NO"};
+            resetButtonRow.Cells.Add(cell);
+
+            htmlTable.Rows.Add(resetButtonRow);
+
+            Controls.Add(htmlTable);
+
+            Session["HtmlTable"] = htmlTable;
+        }
+
+        protected void Button_ServerClick(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("HtmlInputButton", true, false);
+        }
+
+        protected void SubmitButton_ServerClick(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("HtmlInputSubmit", true, false);
+        }
+
+        protected void InputText_ServerChange(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("HtmlInputText", false, true);
+        }
+
+        protected void CheckBox_ServerChange(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("HtmlInputCheckbox", false, true);
+        }
+
+        private void AddRowToHtmlTable(string elementName, bool onServerClick, bool onServerChange)
+        {
+            if (!(Session["HtmlTable"] is HtmlTable htmlTable))
+                return;
+
+            var row = new HtmlTableRow();
+
+            var cell = new HtmlTableCell {InnerHtml = elementName};
+            row.Cells.Add(cell);
+
+            cell = new HtmlTableCell {InnerHtml = onServerClick ? "YES" : "NO"};
+            row.Cells.Add(cell);
+
+            cell = new HtmlTableCell {InnerHtml = onServerChange ? "YES" : "NO"};
+            row.Cells.Add(cell);
+
+            htmlTable.Rows.Add(row);
+
+            Controls.Add(htmlTable);
+        }
+
+        protected void Radio1_ServerChange(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("Radio1", false, true);
+        }
+
+        protected void Radio2_ServerChange(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("Radio2", false, true);
+        }
+
+        protected void TextArea_ServerChange(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("HtmlTextArea", false, true);
+        }
+
+        protected void Select_ServerChange(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("HtmlSelect", false, true);
+        }
+
+        protected void ResetButton_OnServerClick_ServerClick(object sender, EventArgs e)
+        {
+            AddRowToHtmlTable("HtmlInputReset", true, false);
+        }
+    }
+}
