f2b781eaa6b9d487d6649ce1f94bcdcb95601e41
Vitali Filippov <vitaliy.filippov.2012@mail.ru>
2020-04-16 18:42:10 +0300

    Add correct lab1

---
+++ lab_1/lab01N/GetPostHttp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab01
+{
+    public class GetPostHttp : HttpHandler
+    {
+        public override void ProcessRequest(HttpContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            if (request.HttpMethod.Equals("GET"))
+            {
+                HandleGet(request, response);
+            }
+            else if (request.HttpMethod.Equals("POST"))
+            {
+                HandlePost(request, response);
+            }
+            else
+            {
+                response.StatusCode = 405;
+                response.End();
+            }
+        }
+
+        private void HandleGet(HttpRequest request, HttpResponse response)
+        {
+            response.TransmitFile("HtmlPage5.html");
+        }
+
+        private void HandlePost(HttpRequest request, HttpResponse response)
+        {
+            response.Write(int.Parse(request.QueryString["X"]) * int.Parse(request.QueryString["Y"]));
+        }
+    }
+}
---
+++ lab_1/win_client/WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendRequestButton_Click(object sender, EventArgs e)
+        {
+            var request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:23331/sum.fvl");
+            request.Method = "POST";
+
+            byte[] parameters = System.Text.Encoding.UTF8.GetBytes($"Y={textBox1.Text}&X={textBox2.Text}");
+            request.ContentLength = parameters.Length;
+            request.ContentType = "application/x-www-form-urlencoded";
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(parameters, 0, parameters.Length);
+            dataStream.Close();
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var reader = new StreamReader(response.GetResponseStream());
+            ResultTextBox.Text = reader.ReadToEnd();
+        }
+    }
+}
