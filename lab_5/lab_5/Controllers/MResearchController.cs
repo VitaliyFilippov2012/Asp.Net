6424637055828a126732223b49bb7306308938ea
Vitali Filippov <vitaliy.filippov.2012@mail.ru>
2020-04-16 21:48:40 +0300

    Init lab5

---
+++ lab_5/lab_5/Controllers/MResearchController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace lab_5.Controllers
+{
+    public class MResearchController : Controller
+    {
+        // GET: M01
+        public ActionResult M02(string body)
+        {
+            return View();
+        }
+
+        // GET: M02
+        public ActionResult M02(string body)
+        {
+            return View();
+        }
+
+        // GET: M03
+        public ActionResult M03(string body)
+        {
+            return View();
+        }
+
+        // GET: MXX
+        public ActionResult M03(string body)
+        {
+            return View();
+        }
+    }
+}
---
+++ lab_5/lab_5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab_5.MvcApplication" Language="C#" %>
