a021ed9b171acd0fcf5930a7e3cb742079f7b90a
Vitali Filippov <vitaliy.filippov.2012@mail.ru>
2020-04-16 22:08:09 +0300

    Add empty lab5 (task1 + task2)

---
+++ lab_5_task-1/lab_5/Controllers/MResearchController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace lab_5.Controllers
+{
+    public class MResearchController : Controller
+    {
+        // GET: M01
+        public ActionResult M01()
+        {
+            return View();
+        }
+
+        // GET: M02
+        public ActionResult M02()
+        {
+            return View();
+        }
+
+        // GET: M03
+        public ActionResult M03()
+        {
+            return View();
+        }
+
+        // GET: MXX
+        public ActionResult MXX()
+        {
+            return View();
+        }
+    }
+}
---
+++ lab_5_task-2/lab_5/Controllers/CResearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab_5.Controllers
+{
+    public class CResearchController : Controller
+    {
+        // GET - POST: CResearch/ || CResearch/C01
+        [HttpGet]
+        [HttpPost]
+        public ActionResult C01()
+        {
+            return View();
+        }
+
+        // GET - POST: CResearch/C02
+        [HttpGet]
+        [HttpPost]
+        public ActionResult C02()
+        {
+            return View();
+        }
+    }
+}
